d0a173aafef4926c6fb26663dfb85745cabb0f2c
JediahDizon <JediahDizon@JediahDizonLAPTOP.telus>
2017-02-25 17:40:22 -0700

    Some more fixes.

--- controllers/forecastController.js
+++ controllers/forecastController.js
@@ -39,10 +39,9 @@ homepageApp.controller("forecastController", ["$scope", "$http", "$timeout", "op
 			}
 			}).then(function success(response) {
 				$scope.weatherResult = response.data;
-				$scope.weatherResult.message = "";
 				setTimeout(function() { Ladda.stopAll(); }, 500);
 			}, function failure(response) {
-				$scope.weatherResult = "There was an error: " + response + ".\nThis is most likely a browser security functionality that blocks HTTP requests over HTTPS. To see the weather data, please enable it.";
+				$scope.weatherResult = "There was an error: " + response.data + ".\nThis is most likely a browser security functionality that blocks HTTP requests over HTTPS. To see the weather data, please enable it.";
 		});
 	};
 	$scope.convertToCelcius = function(degKelvin) {
