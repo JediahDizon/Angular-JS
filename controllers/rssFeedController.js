3e64badadab3c5b2e7898c9fc40c30c5bd283204
Jediah Dizon <JediahJosuah@gmail.com>
2017-02-26 14:24:35 -0700

    Added a 1 second delay for the RSS Requests.

--- controllers/rssFeedController.js
+++ controllers/rssFeedController.js
@@ -1,17 +1,26 @@
-homepageApp.controller("rssFeedController", ["$scope", "$sce", "$cookies", "rssFeed", function ($scope, $sce, $cookies, rssFeed) {
+homepageApp.controller("rssFeedController", ["$scope", "$timeout", "$cookies", "rssFeed", function ($scope, $timeout, $cookies, rssFeed) {
 	if(!$cookies.get("rssUrl"))
 		$cookies.put("rssUrl", "https://techcrunch.com/feed/");
 	$scope.rssFeedURL = $cookies.get("rssUrl");
+	
+	
+	var rssRequestDelay = 1000;
+	var rssRequestTimeout;
+	
 	$scope.$watch("rssFeedURL", function() {
 		$cookies.put("rssUrl", $scope.rssFeedURL);
-		rssFeed.getFeeds($scope.rssFeedURL).then(function(responseData) {
-			$scope.rssFeeds = responseData.data;
-			angular.forEach($scope.rssFeeds.items, function(rssFeed) {
-				rssFeed.description = $sce.trustAsHtml(decodeURI(rssFeed.description));
+		$timeout.cancel(rssRequestTimeout);
+		
+		rssRequestTimeout = $timeout(function() {
+			rssFeed.getFeeds($scope.rssFeedURL).then(function(responseData) {
+				$scope.rssFeeds = responseData.data;
+				angular.forEach($scope.rssFeeds.items, function(rssFeed) {
+					rssFeed.description = $sce.trustAsHtml(decodeURI(rssFeed.description));
+				});
+			},
+			function(responseError) {
+				console.log("ERROR: " + responseError);
 			});
-		},
-		function(responseError) {
-			console.log("ERROR: " + responseError);
-		});
+		}, rssRequestDelay)
 	});
 }]);
