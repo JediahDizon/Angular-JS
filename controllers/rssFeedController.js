fc8535abbd4b00f612cf7981b244d2f08637a6f7
Jediah Dizon <JediahJosuah@gmail.com>
2017-02-25 18:25:00 -0700

    RSS Url now gets saved by cookies.

--- controllers/rssFeedController.js
+++ controllers/rssFeedController.js
@@ -1,9 +1,9 @@
 homepageApp.controller("rssFeedController", ["$scope", "$sce", "rssFeed", function ($scope, $sce, rssFeed) {
 	if($cookies.get("rssUrl"))
 		$cookies.put("rssUrl", "https://techcrunch.com/feed/");
-	
 	$scope.rssFeedURL = $cookies.get("rssUrl");
 	$scope.$watch("rssFeedURL", function() {
+		$cookies.put("rssUrl", $scope.rssFeedURL);
 		rssFeed.getFeeds($scope.rssFeedURL).$promise.then(function(responseData) {
 			$scope.rssFeeds = responseData;
 			angular.forEach($scope.rssFeeds.items, function(rssFeed) {
