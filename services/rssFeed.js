ebbae70be5676e52a4d21de2831139354050a1f6
Jedia <Jedia@JediahDizonDESKTOP>
2017-03-13 17:42:35 -0600

    No cache for RSS feeds so latest feeds are always obtained.

--- services/rssFeed.js
+++ services/rssFeed.js
@@ -5,6 +5,9 @@ homepageApp.service('rssFeed',['$http', "$sce", function($http, $sce) {
 		return $http({
 			method: "GET",
 			url: "https://api.rss2json.com/v1/api.json",
+			headers: {
+				'Cache-Control' : 'no-cache'
+			},
 			params: {
 					rss_url: rssURL
 		}});
