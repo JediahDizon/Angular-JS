6ca9441bdd7bb3280e746a95639d9917eea066be
Jedia <Jedia@JediahDizonDESKTOP>
2017-08-14 06:15:09 -0600

    Improved Forecast Widget.

--- services/IP API.js
+++ services/IP API.js
@@ -5,14 +5,18 @@ homepageApp.service("IPAPI", ["$http", function($http) {
 	this.longitude = 0;
 	this.latitude = 0;
 	
-	$http({
-		method: "GET",
-		url: "https://ipapi.co/json/"
-	}).then(function success(response) {
+	this.getData = function() {
+		return $http({
+			method: "GET",
+			url: "https://ipapi.co/json/"
+		});
+	};
+	
+	this.getData().then(function success(response) {
 		this.city = response.city;
 		this.region = response.region;
 		this.country = response.country;
 		this.longitude = response.longitude;
 		this.latitude = response.latitude;
-	})();
+	});;
 }]);
